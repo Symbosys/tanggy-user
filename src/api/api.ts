6613e6e779dacff7ce3ec3cde1977464d4e132ec
amitkumardss012 <amitkumaryadav62029@gmail.com>
2026-04-28 14:48:22 +0530

    implemented phone pay configuration

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,8 +3,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { resetToLogin } from '../navigation/NavigationService';
 
 const api = axios.create({
-  baseURL: 'https://api.mintafresh.com/api/v1/minta-fresh',
-  // baseURL: 'http://192.168.1.5:4000/api/v1/minta-fresh',
+  // baseURL: 'https://api.mintafresh.com/api/v1/minta-fresh',
+  baseURL: 'http://192.168.1.8:4000/api/v1/minta-fresh',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
