921b6d59556d174494c2c83c965a5d733c00d2b4
amitkumardss012 <amitkumaryadav62029@gmail.com>
2026-08-05 17:12:50 +0530

    implemented new order changes

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -7,7 +7,7 @@
  */
 
 
-const IS_PRODUCTION = true; // 👈 Toggle this for Production/Local
+const IS_PRODUCTION = false; // 👈 Toggle this for Production/Local
 
 const LOCAL_IP = "192.168.1.5";
 const PROD_DOMAIN = "api.mintafresh.com";
